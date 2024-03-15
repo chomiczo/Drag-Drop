08ebb90592e861c51d9af46fb1a0d010600ada81
xchomiczo <xchomiczo@gmail.com>
2024-03-15 14:38:01 +0100

    ready

--- Drag & Drop/Form1.Designer.cs
+++ Drag & Drop/Form1.Designer.cs
@@ -28,13 +28,103 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 382;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.AllowDrop = true;
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "lista 1 pozycja 1",
+            "lista 1 pozycja 2",
+            "lista 1 pozycja 3",
+            "lista 1 pozycja 4",
+            "lista 1 pozycja 5",
+            "lista 1 pozycja 6",
+            "lista 1 pozycja 7",
+            "lista 1 pozycja 8",
+            "lista 1 pozycja 9",
+            "lista 1 pozycja 10"});
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(382, 450);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox1.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox1_DragOver);
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            // 
+            // listBox2
+            // 
+            this.listBox2.AllowDrop = true;
+            this.listBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Items.AddRange(new object[] {
+            "lista 2 pozycja 1",
+            "lista 2 pozycja 2",
+            "lista 2 pozycja 3",
+            "lista 2 pozycja 4",
+            "lista 2 pozycja 5",
+            "lista 2 pozycja 6",
+            "lista 2 pozycja 7",
+            "lista 2 pozycja 8",
+            "lista 2 pozycja 9",
+            "lista 2 pozycja 10"});
+            this.listBox2.Location = new System.Drawing.Point(0, 0);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox2.Size = new System.Drawing.Size(414, 450);
+            this.listBox2.TabIndex = 0;
+            this.listBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.listBox1_DragDrop);
+            this.listBox2.DragOver += new System.Windows.Forms.DragEventHandler(this.listBox1_DragOver);
+            this.listBox2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Drag & Drop";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
     }
 }
 
--- Drag & Drop/Form1.cs
+++ Drag & Drop/Form1.cs
@@ -12,9 +12,56 @@ namespace Drag___Drop
 {
     public partial class Form1 : Form
     {
+        object dragDropSource = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ListBox lbSender = sender as ListBox;
+            dragDropSource = sender;
+            //int indeks = lbSender.IndexFromPoint(e.Location);
+            if(e.Button == MouseButtons.Left && lbSender.SelectedIndices.Count > 0)
+            {
+                DragDropEffects operacja;
+                operacja = /*lbSender.DoDragDrop(lbSender.Items[indeks], DragDropEffects.Copy | DragDropEffects.Move);*/
+                    lbSender.DoDragDrop(lbSender.SelectedItems, DragDropEffects.Copy | DragDropEffects.Move);
+                if (operacja == DragDropEffects.Move)
+                {
+                    //lbSender.Items.RemoveAt(indeks);
+                    for(int i = lbSender.SelectedIndices.Count - 1; i >= 0; i--)
+                    {
+                        lbSender.Items.Remove(lbSender.SelectedItems[i]);
+                    }
+                }
+                dragDropSource = null;
+            }
+        }
+
+        private void listBox1_DragOver(object sender, DragEventArgs e)
+        {
+            if(dragDropSource == sender)
+                e.Effect = DragDropEffects.None;
+            else
+            if((e.KeyState & 8) == 8)
+                e.Effect = DragDropEffects.Copy;
+            else
+                e.Effect = DragDropEffects.Move;
+        }
+
+        private void listBox1_DragDrop(object sender, DragEventArgs e)
+        {
+            ListBox lbSender = sender as ListBox;
+            ListBox lbSource = dragDropSource as ListBox;
+            int indeks = lbSender.IndexFromPoint(lbSender.PointToClient(new Point(e.X, e.Y)));
+            if (indeks == -1) indeks = lbSender.Items.Count;
+            for(int i = lbSource.SelectedIndices.Count - 1; i >= 0;i--)
+            {
+                lbSender.Items.Insert(indeks, lbSource.Items[lbSource.SelectedIndices[i]]);
+            }
+            //lbSender.Items.Insert(indeks, e.Data.GetData(DataFormats.Text));
         }
     }
 }
